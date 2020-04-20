b54c51545ada30865d5203920a1dc24ea52e8f75
recon <recongamingsam@gmail.com>
2020-04-20 11:26:09 +1000

    cleanup

--- src/main/java/me/reconcubed/communityupdate/init/ModBlocks.java
+++ src/main/java/me/reconcubed/communityupdate/init/ModBlocks.java
@@ -3,7 +3,6 @@ package me.reconcubed.communityupdate.init;
 import me.reconcubed.communityupdate.CommunityUpdate;
 import me.reconcubed.communityupdate.block.*;
 import net.minecraft.block.Block;
-import net.minecraft.block.RedstoneLampBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
