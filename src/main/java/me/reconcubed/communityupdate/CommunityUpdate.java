9fbc83cc2932342de03df63f55dd0ae93e80913e
recon <recongamingsam@gmail.com>
2020-04-23 18:14:44 +1000

    cleanup

--- src/main/java/me/reconcubed/communityupdate/CommunityUpdate.java
+++ src/main/java/me/reconcubed/communityupdate/CommunityUpdate.java
@@ -31,40 +31,31 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(CommunityUpdate.MODID)
 public class CommunityUpdate {
     public static final String MODID = "communityupdate";
 
-//    Vexed Earth
     public static final Tag<EntityType<?>> blacklisted_entities = new EntityTypeTags.Wrapper(new ResourceLocation(MODID, "blacklisted"));
     public static final Tag<Block> spreadable = new BlockTags.Wrapper(new ResourceLocation(MODID, "spreadable"));
-
-
     public static final Tag<Block> paths = new BlockTags.Wrapper(new ResourceLocation(MODID, "paths"));
 
     final ModLoadingContext modLoadingContext = ModLoadingContext.get();
     final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public CommunityUpdate() {
-        // Register Deferred Registers (Does not need to be before Configs)
         ModBlocks.BLOCKS.register(modEventBus);
         ModItems.ITEMS.register(modEventBus);
 //        ModContainerTypes.CONTAINER_TYPES.register(modEventBus);
 //        ModEntityTypes.ENTITY_TYPES.register(modEventBus);
 //        ModTileEntityTypes.TILE_ENTITY_TYPES.register(modEventBus);
 
-        // Register Configs (Does not need to be after Deferred Registers)
         modLoadingContext.registerConfig(ModConfig.Type.CLIENT, ConfigHolder.CLIENT_SPEC);
         modLoadingContext.registerConfig(ModConfig.Type.SERVER, ConfigHolder.SERVER_SPEC);
 
         MinecraftForge.EVENT_BUS.addListener(this::useRoseOnDirt);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
