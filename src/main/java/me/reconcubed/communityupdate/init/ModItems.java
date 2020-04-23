2b56061be8ca0638122e52df80ac401f398a2903
recon <recongamingsam@gmail.com>
2020-04-23 10:50:17 +1000

    Added missing pebble bags & translation keys

--- src/main/java/me/reconcubed/communityupdate/init/ModItems.java
+++ src/main/java/me/reconcubed/communityupdate/init/ModItems.java
@@ -14,4 +14,30 @@ public class ModItems {
             .group(ModItemGroups.COMMUNITY_UPDATE_ITEM_GROUP)
             .maxStackSize(1)
             .defaultMaxDamage(64)));
+
+    public static final RegistryObject<Item> MOSSY_COBBLESTONE_PEBBLES = ITEMS.register("mossy_cobblestone_pebbles", () -> new PebbleBagBaseItem(ModBlocks.MOSSY_COBBLESTONE_PATH.get().getDefaultState(), new Item.Properties()
+            .group(ModItemGroups.COMMUNITY_UPDATE_ITEM_GROUP)
+            .maxStackSize(1)
+            .defaultMaxDamage(64)));
+
+    public static final RegistryObject<Item> ANDESITE_PEBBLES = ITEMS.register("andesite_pebbles", () -> new PebbleBagBaseItem(ModBlocks.ANDESITE_PATH.get().getDefaultState(), new Item.Properties()
+            .group(ModItemGroups.COMMUNITY_UPDATE_ITEM_GROUP)
+            .maxStackSize(1)
+            .defaultMaxDamage(64)));
+
+    public static final RegistryObject<Item> GRANITE_PEBBLES = ITEMS.register("granite_pebbles", () -> new PebbleBagBaseItem(ModBlocks.GRANITE_PATH.get().getDefaultState(), new Item.Properties()
+            .group(ModItemGroups.COMMUNITY_UPDATE_ITEM_GROUP)
+            .maxStackSize(1)
+            .defaultMaxDamage(64)));
+
+    public static final RegistryObject<Item> DIORITE_PEBBLES = ITEMS.register("diorite_pebbles", () -> new PebbleBagBaseItem(ModBlocks.DIORITE_PATH.get().getDefaultState(), new Item.Properties()
+            .group(ModItemGroups.COMMUNITY_UPDATE_ITEM_GROUP)
+            .maxStackSize(1)
+            .defaultMaxDamage(64)));
+
+    public static final RegistryObject<Item> STONE_PEBBLES = ITEMS.register("stone_pebbles", () -> new PebbleBagBaseItem(ModBlocks.STONE_PATH.get().getDefaultState(), new Item.Properties()
+            .group(ModItemGroups.COMMUNITY_UPDATE_ITEM_GROUP)
+            .maxStackSize(1)
+            .defaultMaxDamage(64)));
+
 }
