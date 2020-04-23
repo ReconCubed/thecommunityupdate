d59f9d2abfa13c0a560562ba214423de407852eb
recon <recongamingsam@gmail.com>
2020-04-23 10:45:49 +1000

    Improved PebbleBagBaseItem to accept block & have durability

--- src/main/java/me/reconcubed/communityupdate/init/ModItems.java
+++ src/main/java/me/reconcubed/communityupdate/init/ModItems.java
@@ -10,7 +10,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, CommunityUpdate.MODID);
 
-    public static final RegistryObject<Item> COBBLESTONE_PEBBLES = ITEMS.register("cobblestone_pebbles", () -> new PebbleBagBaseItem(new Item.Properties()
+    public static final RegistryObject<Item> COBBLESTONE_PEBBLES = ITEMS.register("cobblestone_pebbles", () -> new PebbleBagBaseItem(ModBlocks.COBBLESTONE_PATH.get().getDefaultState(), new Item.Properties()
             .group(ModItemGroups.COMMUNITY_UPDATE_ITEM_GROUP)
+            .maxStackSize(1)
             .defaultMaxDamage(64)));
 }
--- src/main/java/me/reconcubed/communityupdate/item/PebbleBagBaseItem.java
+++ src/main/java/me/reconcubed/communityupdate/item/PebbleBagBaseItem.java
@@ -1,52 +1,37 @@
 package me.reconcubed.communityupdate.item;
 
-import me.reconcubed.communityupdate.init.ModBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
 
 public class PebbleBagBaseItem extends Item {
-    public PebbleBagBaseItem(Properties properties) {
-        super(properties);
-    }
+    private BlockState block;
 
-//    @Override
-//    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-//        if (!worldIn.isRemote) {
-//            BlockRayTraceResult rayTraceResult = (BlockRayTraceResult) playerIn.pick(3.0D, 0.0F, false);
-//            BlockPos blockPos = rayTraceResult.getPos().offset(rayTraceResult.getFace());
-//            playerIn.sendMessage(new TranslationTextComponent(blockPos.toString()));
-//
-//        }
-//        return super.onItemRightClick(worldIn, playerIn, handIn);
-//    }
+    public PebbleBagBaseItem(BlockState blockState, Properties properties) {
+        super(properties);
+
+        block = blockState;
+    }
 
     @Override
     public ActionResultType onItemUse(ItemUseContext context) {
         if (!context.getWorld().isRemote) {
             PlayerEntity playerEntity = context.getPlayer();
-
             if (playerEntity != null) {
-                BlockPos blockPos = context.getPos().offset(context.getFace());
-                playerEntity.sendMessage(new TranslationTextComponent(blockPos.toString()));
+                BlockPos blockPos = context.getPos();
                 BlockState blockState = context.getWorld().getBlockState(blockPos);
 
-                if (!blockState.isReplaceable((BlockItemUseContext) context)) {
+                if (!blockState.isReplaceable(new BlockItemUseContext(context))) {
                     blockPos = blockPos.offset(context.getFace());
                 }
 
-                context.getWorld().setBlockState(blockPos, ModBlocks.COBBLESTONE_PATH.get().getDefaultState());
-
+                context.getWorld().setBlockState(blockPos, this.block);
+                context.getItem().damageItem(1, playerEntity, (p) ->
+                        p.sendBreakAnimation(context.getHand()));
             }
             return ActionResultType.SUCCESS;
         } else {
