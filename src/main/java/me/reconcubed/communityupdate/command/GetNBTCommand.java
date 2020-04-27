436b66e40435aa169a28e40879795e2afb79fb9e
recon <recongamingsam@gmail.com>
2020-04-27 12:35:12 +1000

    removed imports

--- src/main/java/me/reconcubed/communityupdate/command/GetNBTCommand.java
+++ src/main/java/me/reconcubed/communityupdate/command/GetNBTCommand.java
@@ -1,20 +1,15 @@
 package me.reconcubed.communityupdate.command;
 
-import com.google.common.base.Joiner;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.dimension.DimensionType;
 
 public class GetNBTCommand implements Command<CommandSource> {
     private static final GetNBTCommand CMD = new GetNBTCommand();
@@ -28,6 +23,7 @@ public class GetNBTCommand implements Command<CommandSource> {
     @Override
     public int run(CommandContext<CommandSource> context) throws CommandSyntaxException {
         CommandSource commandSource = context.getSource();
+
         if ((commandSource.getEntity() instanceof PlayerEntity)) {
             ItemStack heldItemMainhand = ((PlayerEntity) commandSource.getEntity()).getHeldItemMainhand();
 
